fa0d65730358ff5f0ce688e76de727d019ed9aa3
narendravs <narendravs_mca@yahoo.com>
2025-07-22 21:21:48 +0530

    created the patient form to store patient details using appwrite api's

---
+++ src/components/forms/PatientForm.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { UserFormValidation } from "@/lib/validation";
+import { Form, FormField } from "@/components/ui/form";
+import { createUser } from "@/lib/actions/patient.actions";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+    try {
+      const user = await createUser({
+        email: data.email,
+        phone: data.phone,
+        name: data.name,
+      });
+      if (user) {
+        router.push(`/patients/${user.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container flex flex-col space-y-4 border-1 rounded-lg bg-white-500 w-full p-10"
+      >
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Create Patient</h1>
+          <p className="text-dark-700">Get started with appointments.</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+        <div className="flex items-center justify-content-center">
+          <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
