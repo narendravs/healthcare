414ce5cf873b133cf6fdca8d1372871f4febabbf
narendravs <narendravs_mca@yahoo.com>
2025-08-13 12:37:53 +0530

    created the configuration file to connect to the Appwrite database using its keys and builtin api's

---
+++ src/lib/actions/appwrite.config.ts
@@ -0,0 +1,22 @@
+import * as sdk from "node-appwrite";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT!;
+export const PROJECT_ID = process.env.PROJECT_ID!;
+export const API_KEY = process.env.API_KEY!;
+export const DATABASE_ID = process.env.DATABASE_ID!;
+export const PATIENT_COLLECTION_ID = process.env.PATIENT_COLLECTION_ID!;
+export const DOCTOR_COLLECTION_ID = process.env.DOCTOR_COLLECTION_ID!;
+export const APPOINTMENT_COLLECTION_ID = process.env.APPOINTMENT_COLLECTION_ID!;
+export const BUCKET_ID = process.env.NEXT_PUBLIC_BUCKET_ID!;
+export const BUCKET_URL = process.env.NEXT_PUBLIC_BUCKET_URL!;
+
+export const client = new sdk.Client();
+
+client.setEndpoint(ENDPOINT).setProject(PROJECT_ID).setKey(API_KEY);
+
+export const databases = new sdk.Databases(client);
+export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
