33fd9703db9511414f45568e2266041f1e5deaee
narendravs <narendravs_mca@yahoo.com>
2026-05-04 18:29:23 +0530

    fix(types): add module declarations for library and global CSS

--- additional-env.d.ts
+++ additional-env.d.ts
@@ -1,1 +1,7 @@
 declare module "react-phone-number-input/style.css";
+declare module "react-datepicker/dist/react-datepicker.css";
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
